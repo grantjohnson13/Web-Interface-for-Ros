8f252b8a0c37a8e21fdf6d59d5150fdee183d0dd
grantjohnson13 <86679297+grantjohnson13@users.noreply.github.com>
2021-10-04 09:22:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,32 +3,19 @@ let vueApp = new Vue({
     data: {
         // ros connection
         ros: null,
-        rosbridge_address: 'wss://i-0734dfc7411934198.robotigniteacademy.com/rosbridge/',
+        rosbridge_address: 'wss://i-0edfd58971bf7a79c.robotigniteacademy.com/rosbridge/',
         connected: false,
+        // subscriber data
+        position: { x: 0, y: 0, z: 0, },
+        // fence mode
+        fenceMode: false,
+        insideFence: false,
         // page content
         menu_title: 'Connection',
-        // dragging data
-        dragging: false,
-        x: 'no',
-        y: 'no',
-        dragCircleStyle: {
-            margin: '0px',
-            top: '0px',
-            left: '0px',
-            display: 'none',
-            width: '75px',
-            height: '75px',
-        },
-        // joystick valules
-        joystick: {
-            vertical: 0,
-            horizontal: 0,
-        },
-        // publisher
-        pubInterval: null,
+        main_title: 'Main title, from Vue!!',
     },
     methods: {
-        connect: function () {
+        connect: function() {
             // define ROSBridge connection object
             this.ros = new ROSLIB.Ros({
                 url: this.rosbridge_address
@@ -38,7 +25,18 @@ let vueApp = new Vue({
             this.ros.on('connection', () => {
                 this.connected = true
                 console.log('Connection to ROSBridge established!')
-                this.pubInterval = setInterval(this.publish, 100)
+                let topic = new ROSLIB.Topic({
+                    ros: this.ros,
+                    name: '/odom',
+                    messageType: 'nav_msgs/Odometry'
+                })
+                topic.subscribe((message) => {
+                    this.position = message.pose.pose.position
+                    console.log(`fence mode is ${this.fenceMode}`)
+                    if (this.fenceMode) {
+                        this.stayOnTheFence(message.pose.pose.position)
+                    }
+                })
             })
             this.ros.on('error', (error) => {
                 console.log('Something went wrong when trying to connect')
@@ -47,25 +45,12 @@ let vueApp = new Vue({
             this.ros.on('close', () => {
                 this.connected = false
                 console.log('Connection to ROSBridge was closed!')
-                clearInterval(this.pubInterval)
             })
         },
-        publish: function () {
-            let topic = new ROSLIB.Topic({
-                ros: this.ros,
-                name: '/cmd_vel',
-                messageType: 'geometry_msgs/Twist'
-            })
-            let message = new ROSLIB.Message({
-                linear: { x: this.joystick.vertical, y: 0, z: 0, },
-                angular: { x: 0, y: 0, z: this.joystick.horizontal, },
-            })
-            topic.publish(message)
-        },
-        disconnect: function () {
+        disconnect: function() {
             this.ros.close()
         },
-        sendCommand: function () {
+        sendCommand: function() {
             let topic = new ROSLIB.Topic({
                 ros: this.ros,
                 name: '/cmd_vel',
@@ -77,49 +62,60 @@ let vueApp = new Vue({
             })
             topic.publish(message)
         },
-        startDrag() {
-            this.dragging = true
-            this.x = this.y = 0
+        turnRight: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 1, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: -0.5, },
+            })
+            topic.publish(message)
         },
-        stopDrag() {
-            this.dragging = false
-            this.x = this.y = 'no'
-            this.dragCircleStyle.display = 'none'
-            this.resetJoystickVals()
+        stop: function() {
+            let topic = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            let message = new ROSLIB.Message({
+                linear: { x: 0, y: 0, z: 0, },
+                angular: { x: 0, y: 0, z: 0, },
+            })
+            topic.publish(message)
         },
-        doDrag(event) {
-            if (this.dragging) {
-                this.x = event.offsetX
-                this.y = event.offsetY
-                let ref = document.getElementById('dragstartzone')
-                this.dragCircleStyle.display = 'inline-block'
-
-                let minTop = ref.offsetTop - parseInt(this.dragCircleStyle.height) / 2
-                let maxTop = minTop + 200
-                let top = this.y + minTop
-                this.dragCircleStyle.top = `${top}px`
-
-                let minLeft = ref.offsetLeft - parseInt(this.dragCircleStyle.width) / 2
-                let maxLeft = minLeft + 200
-                let left = this.x + minLeft
-                this.dragCircleStyle.left = `${left}px`
-
-                this.setJoystickVals()
+        switchFenceMode: function() {
+            this.fenceMode = !this.fenceMode
+        },
+        stayOnTheFence: function(position) {
+            let topicToPublish = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/cmd_vel',
+                messageType: 'geometry_msgs/Twist'
+            })
+            if (position.x > -5 && position.x < 5 && position.y > -5 && position.y < 5) {
+                // we are inside the fence!
+                this.insideFence = true
+                let message = new ROSLIB.Message({
+                    linear: { x: 0.5, y: 0, z: 0, },
+                    angular: { x: 0, y: 0, z: 0, },
+                })
+                topicToPublish.publish(message)
+            } else {
+                // we are outside the fence!
+                this.insideFence = false
+                let message = new ROSLIB.Message({
+                    linear: { x: 0.5, y: 0, z: 0, },
+                    angular: { x: 0, y: 0, z: 0.5, },
+                })
+                topicToPublish.publish(message)
             }
-        },
-        setJoystickVals() {
-            this.joystick.vertical = -1 * ((this.y / 200) - 0.5)
-            this.joystick.horizontal = +1 * ((this.x / 200) - 0.5)
-        },
-        resetJoystickVals() {
-            this.joystick.vertical = 0
-            this.joystick.horizontal = 0
         },
     },
     mounted() {
         // page is ready
-        window.addEventListener('mouseup', this.stopDrag)
+        console.log('page is ready!')
     },
 })
-vueApp.data = vueApp.data
-vueApp.startDrag = vuepApp.startDrag
